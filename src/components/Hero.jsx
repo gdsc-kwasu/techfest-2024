5fa3b6699652d2c17dee7a050d5f9528ba2f59fe
Pariola Ololade <96139999+loppam@users.noreply.github.com>
2024-06-29 13:52:02 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,47 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 const Hero = () => {
-  const [expiryTime, setExpiryTime] = useState("29 jun 2024 09:00:00");
-  const [countdownTime, setCountdownTime] = useState({
-    countdownDays: "",
-    countdownHours: "",
-    countdownMinutes: "",
-    countdownSeconds: "",
-  });
-
-  const countdownTimer = () => {
-    const timeInterval = setInterval(() => {
-      const countdownDateTime = new Date(expiryTime).getTime();
-      const currentTime = new Date().getTime();
-      const remainingDayTime = countdownDateTime - currentTime;
-      const totalDays = Math.floor(remainingDayTime / (1000 * 60 * 60 * 24));
-      const totalHours = Math.floor(
-        (remainingDayTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const totalMinutes = Math.floor(
-        (remainingDayTime % (1000 * 60 * 60)) / (1000 * 60)
-      );
-      const totalSeconds = Math.floor((remainingDayTime % (1000 * 60)) / 1000);
-
-      const runningCountdownTime = {
-        countdownDays: totalDays,
-        countdownHours: totalHours,
-        countdownMinutes: totalMinutes,
-        countdownSeconds: totalSeconds,
-      };
-
-      setCountdownTime(runningCountdownTime);
-
-      if (remainingDayTime < 0) {
-        clearInterval(timeInterval);
-        setExpiryTime(false);
-      }
-    }, 1000);
-  };
-
-  useEffect(() => {
-    countdownTimer();
-  });
+  
   return (
     <div className="hero">
       <div className="content">
@@ -54,19 +14,19 @@ const Hero = () => {
             </h1>
             <div className="countdown">
               <div className="timer">
-                <h2>{countdownTime.countdownDays}</h2>
+                <h2>0</h2>
                 <h3>Days</h3>
               </div>
               <div className="timer">
-                <h2>{countdownTime.countdownHours}</h2>
+                <h2>0</h2>
                 <h3>Hours</h3>
               </div>
               <div className="timer">
-                <h2>{countdownTime.countdownMinutes}</h2>
+                <h2>0</h2>
                 <h3>minutes</h3>
               </div>
               <div className="timer">
-                <h2>{countdownTime.countdownSeconds}</h2>
+                <h2>0</h2>
                 <h3>seconds</h3>
               </div>
             </div>
